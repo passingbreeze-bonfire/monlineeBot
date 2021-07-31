04d02bfa5a057c61903689a078fe90e4500aeecc
JeongMin <jungmin1237@pusan.ac.kr>
2021-07-31 18:41:32 +0900

    save lotto result

--- bot_lotto.py
+++ bot_lotto.py
@@ -1,4 +1,4 @@
-import json, datetime as dt, heapq as pq, io
+import json, datetime as dt, heapq as pq, os
 
 import pandas as pd, numpy as np, discord, requests
 from discord.ext import commands
@@ -35,19 +35,23 @@ class bot_lottery(commands.Cog):
         else:
             await ctx.send("새로운 당첨 번호가 나오지 않았습니다.")
             await ctx.send(f"{self.__latest['round'][0]}회차 당첨 번호까지 봤을 때...")
-            max_n = np.int64(10_0000_0000)
-            np.random.seed(int(self.__seed))
-            rand_num = np.random.binomial(max_n, 1/8_145_060, size = (5, 45))
-            for arr in rand_num:
-                for i, cnt in enumerate(arr, 1):
-                    pq.heappush(win_q, (-cnt, i))
-                while win_q:
-                    if len(msg) == 6:
-                        msg = "\t> **{}**\n".format("\t".join(msg))
-                        msgs.append(msg)
-                        msg = []
-                        break
-                    msg.append(str(pq.heappop(win_q)[1]))
+            if not os.path.exists('last_result.npy'):
+                max_n = np.int64(10_0000_0000)
+                np.random.seed(int(self.__seed))
+                rand_num = np.random.binomial(max_n, 1/8_145_060, size = (5, 45))
+                for arr in rand_num:
+                    for i, cnt in enumerate(arr, 1):
+                        pq.heappush(win_q, (-cnt, i))
+                    while win_q:
+                        if len(msg) == 6:
+                            msg = "\t> **{}**".format("\t".join(msg))
+                            msgs.append(msg)
+                            msg = []
+                            break
+                        msg.append(str(pq.heappop(win_q)[1]))
+                np.save('last_result.npy', np.array(msgs, dtype=np.str))
+            else:
+                msgs = np.load('last_resslut.npy')
             await ctx.send('\n'.join(msgs))
 
 
